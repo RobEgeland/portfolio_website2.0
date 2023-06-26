ac43c0ede7c48a3e8f3ed62cf07785a65c4e8c74
Robert Egeland <robegeland@yahoo.com>
2023-06-26 17:21:32 -0500

    fist commit on second computer

--- src/app/Components/NavBar.js
+++ src/app/Components/NavBar.js
@@ -59,7 +59,7 @@ const NavBar = () => {
                         </div>
                     </div>
                     <div className='border-b border-gray-300 my-4'>
-                        <p className='w-[85%] md:w-[90%] py-4'>PLaceholder for quote</p>
+                        <p className='w-[85%] md:w-[90%] py-4'>Testinf other computer</p>
                     </div>
                 </div>
                 <div className='py-4 flex flex-col'>
